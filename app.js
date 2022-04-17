ce3ef9591cb3fc79402320c676305109e8af8da2
Alex A <antonich@oakland.edu>
2022-04-17 14:48:28 -0400

    added comments to app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// download nodejs dependencies
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,17 +10,21 @@ var thumbler = require('video-thumb');
 // console.log(videos);
 
 var multer = require('multer');
+// define multer function
 var storage = multer.diskStorage({
   destination: (req,file,cb) =>{
     cb(null, './public/videos');
+    //put uploaded video files in videos folder
   },
   filename: (req, file, cb) => {
     var {originalname} = file;
     cb(null,originalname);
+    //make the name of the video in the folder the same as the uploaded file
   }
 });
 var upload = multer({storage});
 
+//define routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -29,6 +34,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//ejs middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -39,31 +45,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use("/public", express.static(__dirname + "/public"));
 
+//render index page
 app.get("/", (req, res) => {
+  //get array of videos from videos folder
   var videos = fs.readdirSync('./public/videos');
-  videos.forEach(video => {
-    console.log(video);
-  });
+  //render index page and pass list of videos
   res.render("index", {videos: videos});
 });
 
 app.get("/player", (req, res) => {
+  //get array of videos from videos folder
   var videos = fs.readdirSync('./public/videos');
+  //render player page and pass list of videos and the file in the url
   res.render("player", {load: req.query.file, videos: videos});
 });
 
 app.get("/upload", (req, res) => {
+  //render upload page
   res.render("upload");
 });
 
 //upload file api
 app.post("/uploadthis", upload.single('UploadVideo'), (req, res) => {
   console.log('./public/videos/' + req.file.filename);
+  // extract thumbnail from uploaded video, put it in img folder
   thumbler.extract('./public/videos/' + req.file.filename, './public/img/' + req.file.filename + '.png', '00:00:01', '640x360', function(){
     console.log('snapshot saved to ' + './public/img/' + req.file.filename + '.png' + ' (640x360) with a frame at 00:00:01');
   });
+  // there has to be a wait otherwise the thumbnails wont load lol
   setTimeout(function(){
+    //get array of videos from videos folder
     var videos = fs.readdirSync('./public/videos');
+    //render index page and pass list of videos
     res.render("index", {videos: videos});
   },500);
 });
